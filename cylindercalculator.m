61aac2ad0de7e12ed8dd641e06e67b08816d1b0b
RobertZruban <73717057+RobertZruban@users.noreply.github.com>
2022-05-26 20:55:45 +0200

    Delete cylindercalculator.m

--- cylindercalculator.m
+++
@@ -1,8 +0,0 @@
-function [volume, surface] = cylindercalculator()
-radius = input("Enter a radius here:");
-height = input("Enter a height here:");
-volume = pi * radius^2 * height;
-surface = 2 * pi * radius * height;
-fprintf("Volume of a cylinder is %.2f \n", volume)
-fprintf("Surface of a cylinder is %.2f \n", surface)
-end
